c8ca58e3f5ae84848f39be6bc14a5470e24e19cb
93romano <93romano@gmail.com>
2025-09-09 17:48:55 +0900

    Initial Commit

---
+++ mini-chess/mini_chessApp.swift
@@ -0,0 +1,17 @@
+//
+//  mini_chessApp.swift
+//  mini-chess
+//
+//  Created by 김동혁 on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct mini_chessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mini-chessTests/mini_chessTests.swift
@@ -0,0 +1,17 @@
+//
+//  mini_chessTests.swift
+//  mini-chessTests
+//
+//  Created by 김동혁 on 9/9/25.
+//
+
+import Testing
+@testable import mini_chess
+
+struct mini_chessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
